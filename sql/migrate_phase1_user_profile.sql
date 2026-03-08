561f4dd1d79740cc6a85866a7f92605d734b1c01
Sirleafcarl <sirleafcarl@gmail.com>
2026-03-08 17:04:20 +0800

    数据库更新

--- sql/migrate_phase1_user_profile.sql
+++
@@ -1,8 +0,0 @@
-﻿-- Phase 1: 用户资料字段扩展
--- 执行前请确保已连接 cheese 数据库
-
-ALTER TABLE `t_user`
-    ADD COLUMN `nickname` varchar(60)  DEFAULT NULL COMMENT '昵称' AFTER `username`,
-    ADD COLUMN `avatar`   varchar(256) DEFAULT NULL COMMENT '用户头像URL' AFTER `nickname`,
-    ADD COLUMN `email`    varchar(128) DEFAULT NULL COMMENT '邮箱' AFTER `avatar`,
-    ADD COLUMN `bio`      varchar(256) DEFAULT NULL COMMENT '个人简介' AFTER `email`;
